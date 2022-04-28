f949787906ced7e36f99470ed9d6574f39b1b628
Jan Vavřínek <janvavrinek1@gmail.com>
2022-04-28 16:05:56 +0200

    Fix mobile.twitter.com links not being recognized

--- EvenHandlers/OnMessageCreate/index.ts
+++ EvenHandlers/OnMessageCreate/index.ts
@@ -1,6 +1,6 @@
 import { Client, Message } from "discord.js";
 
-const REGEX = /(https?:\/\/twitter\.com\/\S+\/status\/[0-9]+)/g;
+const REGEX = /(https?:\/\/(mobile.)?twitter\.com\/\S+\/status\/[0-9]+)/g;
 
 /**
  * Handle messages and transforms Twitter embeds into TwitFix ones
@@ -11,7 +11,11 @@ const OnMessageCreate = (msg: Message, client: Client) => {
   if (msg.channel.type === "GUILD_TEXT" && msg.author.id !== client.user?.id) {
     const matches = msg.content.match(REGEX);
     const res = matches?.reduce(
-      (p, c) => `${p}${c.replace("twitter", "fxtwitter").replace(/\/$/, "")}\n`, //replace twitter with fxtwitter and then remove trailing slash
+      (p, c) =>
+        `${p}${c
+          .replace(/mobile\./, "") //remove mobile. from the link
+          .replace("twitter", "fxtwitter") //replace twitter with fxtwitter
+          .replace(/\/$/, "")}\n`, // remove trailing slash because otherwise the link doesn't work
       ""
     );
     if (res && res !== "") {
